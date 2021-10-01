e993c35217d5b056de9f58505f85e3ba0bdbff18
kanagaraju <kanagarajup@gmail.com>
2021-10-01 22:46:43 +0530

    update todo using PATCH

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,7 @@
 # todos_controller.rb
 class TodosController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     #render plain: "Hello, this is /todos! #{DateTime.now}"
     render plain: Todo.all.map { |todo| todo.to_pleasant_string }.join("\n")
@@ -17,5 +19,14 @@ class TodosController < ApplicationController
     new_todo = Todo.create!(todo_text: todo_text, due_date: due_date, completed: false)
     response_text = "Hey, your new todo is created with the id #{new_todo.id} "
     render plain: response_text
+  end
+
+  def update
+    id = params[:id]
+    completed = params[:completed]
+    todo = Todo.find(id)
+    todo.completed = completed
+    todo.save!
+    render plain: "Updated todo completed status to #{completed}"
   end
 end
